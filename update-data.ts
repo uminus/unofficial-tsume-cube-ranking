df55e9c462e6e9aed26ced62e4f0e0f46bbdc8fa
uminus <ucon.ucon@gmail.com>
2021-05-08 22:50:41 +0900

    fix: regex
    (cherry picked from commit 79cf21243d62d7a6806169f794ca59b1a83685a2)

--- update-data.ts
+++ update-data.ts
@@ -12,7 +12,7 @@ function ignoreTweet(t: Tweet): boolean {
 }
 
 const tweets = await fetchTweets(lastUpdatedAt);
-const regex = /(?<comment1>[\s\S]*)(?<te>\d+)手の(?<is_virtual>バーチャル)?詰めキューブを(?<is_only_scramble_image>.*スクランブル画像のみで)?(?<time>(\d+:)?\d*\.\d*)で[\s\S]*解いた問題: (?<scramble>[UDFBLR' 2]*)[\s\S]*(#詰めキューブ)?(?<comment2>[\s\S]*)/;
+const regex = /(?<comment1>[\s\S]*)(?<te>\d+)手の(?<is_virtual>バーチャル)?詰めキューブを(?<is_only_scramble_image>.*スクランブル画像のみで)?(?<time>(\d+:)?\d*\.\d*)で[\s\S]*解いた問題: (?<scramble>[UDFBLR' 2]*)[\s\S]*(#詰めキューブ)(?<comment2>[\s\S]*)/;
 const solves = (tweets.data || [])
   .filter(ignoreTweet)
   .map(t => {
